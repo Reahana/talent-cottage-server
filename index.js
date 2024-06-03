58a35244852695cd791983baf82f9c4e539620f4
Reahana <reahana233@gmail.com>
2024-06-03 10:32:21 +0600

    instructors

--- index.js
+++ index.js
@@ -10,7 +10,74 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
- 
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9jxj7jj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+/// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+      const classCollection = client.db("talentDB").collection("classes");
+      const instructorCollection = client.db("talentDB").collection("instructors");
+
+
+
+        //Class collection 
+    app.get('/classes', async(req, res) =>{
+        const result = await classCollection.find().toArray();
+        res.send(result);
+      })
+      
+      app.get('/top-classes', async(req, res) =>{
+        
+        const allClasses = await classCollection.find({}).toArray();
+        const topClasses = allClasses
+        .map(cls => ({
+          ...cls,
+          studentCount: cls.students
+        }))
+        .sort((a, b) => b.studentCount - a.studentCount)
+         .slice(0, 6);
+    
+        res.send(topClasses);
+    })
+  
+    app.post('/class',  async (req, res) => {
+      const item = req.body;
+      const result = await classCollection.insertOne(item);
+      res.send(result);
+    });
+  
+    app.delete('/class/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await classCollection.deleteOne(query);
+      res.send(result);
+    })
+
+//instructors
+app.get('/instructors', async(req, res) =>{
+  const result = await instructorCollection.find().toArray();
+  res.send(result);
+})
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+     // await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 
